53b098fda806c30f73030c7fc2767636caebf949
Marvine Gaudrée <marvine.gaudree@stadjutor.com>
2022-04-07 09:10:15 +0200

    <frontend>(Image): Modif positions/buttons/image

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-let langages_list = ['java', 'javascript', 'c#', 'c', 'c++', 'python',
-    'ruby', 'php', 'swift', 'julia', 'bash', 'css', 'html',
-    'haskell', 'kotlin#', '.net', 'powershell', 'perl', 'rust'
+let langages_list = ['java', 'javascript', 'c#', 'c', 'c++', 'python', 'ruby', 'php', 'swift', 'julia', 'bash',
+    'css', 'html', 'haskell', 'kotlin#', '.net', 'powershell', 'perl', 'rust', 'basic', 'visual basic', 'assembleur',
+    'sql', 'swift', 'r', 'go', 'pascal', 'matlab', 'fortran', 'dart', 'bosque', 'java fx', 'k'
 ]
 let animaux_list = ['chien', 'chat', 'lion', 'tigre', 'serpent',
-    'lezard', 'girafe', 'elephant', 'cochon', 'vache', 'requin', 'baleine', 'rat', 'souris'
+    'lezard', 'girafe', 'éléphant', 'cochon', 'vache', 'requin', 'baleine', 'rat', 'souris', 'aigle', 'alligator',
+    'antilope', 'autruche', 'babouin', 'singe', 'belette', 'béluga', 'bison', 'bonobo', 'cachalot', 'caiman', 'caribou',
+    'cerf', 'castor', 'chamois', 'chevreuil', 'chimpanzé', 'cougar', 'crapaud', 'coyote', 'crocodile', 'dauphin',
+    'élan', 'fennec', 'fouine', 'fourmis', 'guépard', 'hérisson', 'hibou', 'hirondelle', 'iguane', 'koala', 'jaguar', 'impala',
+    'kangourou', 'léopard', 'loup', 'lynx', 'lapin', 'lièvre', 'tortue', 'béluga', 'belette', 'biche', 'buffle',
+    'caribou', 'coyote', 'chouette', 'castor', 'pigeon', 'perroquet', 'flanant rose', 'hoppopotame', 'thon', 'narval',
+    'orque', 'ours', 'panda', 'puma', 'renne', 'raton laveur', 'salamandre', 'tapir', 'wapiti', 'zèbre', 'cheval',
+    'âne', 'poney'
 ]
-let pays_list = ['france', 'espagne', 'italie', 'grece', 'maroc', 'turquie', 'allemagne', 'autriche',
-    'belgique', 'bulgarie', 'chypre', 'croatie'
+let pays_list = ['france', 'espagne', 'italie', 'grece', 'maroc', 'turquie', 'allemagne', 'autriche', 'belgique',
+    'bulgarie', 'chypre', 'croatie', 'albanie', 'algérie', 'andorre', 'angola', 'argentine', 'arménie', 'australie',
+    'bahamas', 'bangladesh', 'belize', 'bénin', 'birmanie', 'bolivie', 'burkina', 'burkina faso', 'cambodge', 'cameroun',
+    'canada', 'chili', 'chine', 'chypre', 'colombie', 'congo', 'corée', 'corée du nord', 'corée du sud', 'costa rica',
+    'costa rica', 'cuba', 'danemark', 'djibouti', 'dominique', 'égypte', 'équateur', 'estonie', 'états-unis', 'éthiopie',
+    'finlande', 'gabon', 'géorgie', 'ghana', 'grèce', 'inde', 'irlande', 'islande', 'japon', 'russie', 'royaume-uni',
+    'brésil', 'angleterre', 'afrique'
 ]
 
 let blank_list_langage = []
@@ -25,7 +37,7 @@ function generateTheme(list, blank) {
             list.splice(i, 1)
             score += 1
             totalscore.innerHTML = score
-            addTime(2)
+            addTime(6)
             console.log("LISTE : ", list)
             console.log("BLANK : ", blank)
             trouve = true;
@@ -75,11 +87,13 @@ var timeLeft = 60; // seconds
 const temps = document.getElementById("timer");
 const image = document.getElementById("image");
 
-
+const music2 = new Audio('./Sound/bruit2.wav');
 // What to do when the timer runs out
 function gameOver() {
     clearInterval(timer)
     btn.disabled = true
+    music2.play();
+    music2.loop = false;
 }
 
 function addTime(value) {
@@ -99,10 +113,22 @@ function removeTime(value) {
     }
 }
 
+const music1 = new Audio('./Sound/bruit.wav');
+let bruit1 = false
+
 function updateTimer() {
     timeLeft = timeLeft - 1;
-    let transparence = (100 - timeLeft) / 100;
+
+    let transparence = ((timeLeft * 100) / 60) / 100;
+    console.log(transparence)
     image.style.opacity = transparence;
+
+    if (!bruit1 && timeLeft < 10) {
+        music1.play();
+        music1.loop = false;
+        bruit1 = true
+    }
+
     if (timeLeft >= 0)
         temps.innerHTML = timeLeft
     else {
